5e8f3b78ea8680dcf32090b27d8b6c6107990752
Pedro Fernandes <pedro.fernandes@bix-tech.com>
2024-05-15 14:19:36 -0300

    chore: Configure Django settings in asgi.py

--- mindsupport/asgi.py
+++ mindsupport/asgi.py
@@ -6,15 +6,17 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
-
 import os
+import django
+
+# Configure Django settings
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mindsupport.settings")
+django.setup()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from django.urls import re_path
 from api.consumers import TextRoomConsumer
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 websocket_urlpatterns = [
     re_path(r"^ws/(?P<sala>[^/]+)/$", TextRoomConsumer.as_asgi()),
